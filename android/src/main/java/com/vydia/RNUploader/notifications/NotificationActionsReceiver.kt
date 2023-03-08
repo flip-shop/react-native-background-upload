f79192efce4820b14444865f6d0dbe3db08837de
Krzysztof Gajewski <krzysztof.gajewski@flip.shop>
2023-03-08 17:28:54 +0100

    MOB-3176 Cancelling upload through notification

--- android/src/main/java/com/vydia/RNUploader/notifications/NotificationActionsReceiver.kt
+++ android/src/main/java/com/vydia/RNUploader/notifications/NotificationActionsReceiver.kt
@@ -3,24 +3,27 @@ package com.vydia.RNUploader.notifications
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import android.util.Log.d
 import com.vydia.RNUploader.notifications.data.ACTION_UPLOAD_CANCEL
 import com.vydia.RNUploader.notifications.data.PARAM_UPLOAD_ID
+import com.vydia.RNUploader.worker.UploadWorkerManager
+import com.vydia.RNUploader.worker.UploadWorkerManagerImpl
+import org.koin.java.KoinJavaComponent.inject
 
 private const val TAG = "NotificationActReceiver"
-class NotificationActionsReceiver(
-  private val cancelActionReceived: (uploadId: String) -> Unit
-): BroadcastReceiver() {
+class NotificationActionsReceiver: BroadcastReceiver() {
+
+  private val uploadWorkerManager: UploadWorkerManager
+    by inject(UploadWorkerManagerImpl::class.java)
 
   override fun onReceive(context: Context?, intent: Intent?) {
-    d(TAG, "onReceive")
+
     if(intent == null || context == null) {
       return
     }
-    d(TAG, "onReceive intent = $intent")
+
     if(intent.action == ACTION_UPLOAD_CANCEL) {
       intent.getStringExtra(PARAM_UPLOAD_ID)?.let {
-        cancelActionReceived(it)
+        uploadWorkerManager.cancelWorker(it)
       }
     }
   }
