f3deea85d8adf8d6aac87be0aeb81621baf01d98
Michael Czerniakowski <michal.czerniakowski@flip.shop>
2023-05-19 20:03:39 +0200

    Create empty Swift file and corresponding bridging header.

---
+++ ios/FileUploaderService.swift
@@ -0,0 +1,9 @@
+//
+//  FileUploaderService.swift
+//  VydiaRNFileUploader
+//
+//  Created by Michael Czerniakowski on 19/05/2023.
+//  Copyright © 2023 Marc Shilling. All rights reserved.
+//
+
+import Foundation
