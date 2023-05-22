fdb788197d045729fbae6d43877ed24809773f1a
Michael Czerniakowski <michal.czerniakowski@flip.shop>
2023-05-22 15:27:03 +0200

    Add try-catch for better error readability.

--- ios/FileUploaderService.swift
+++ ios/FileUploaderService.swift
@@ -31,14 +31,13 @@ public class FileUploaderService: NSObject, URLSessionDelegate {
     public override init() { //WIP!!!
         self._fileManager = FileManager()
     }
-
+    
     static let BACKGROUND_SESSION_ID: String = "ReactNativeBackgroundUpload"
     
-    func saveMultipartUploadDataToDisk(uploadId: String, data: Data) -> URL? {
-        
+    func saveMultipartUploadDataToDisk(uploadId: String, data: Data) throws -> URL {
         let paths = FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask)
         guard let cacheDirectory = paths.first else {
-            return nil
+            throw UploadError.directoryCreationFailed
         }
         
         let path = "\(uploadId).multipart"
@@ -47,39 +46,41 @@ public class FileUploaderService: NSObject, URLSessionDelegate {
         
         print("Path to save: \(filePath.path)")
         
+        let fileManager = FileManager.default
+        
         // Remove file if needed
-        if _fileManager.fileExists(atPath: filePath.path) {
+        if fileManager.fileExists(atPath: filePath.path) {
             do {
-                try _fileManager.removeItem(at: filePath)
+                try fileManager.removeItem(at: filePath)
             } catch {
                 print("Cannot delete file at path: \(filePath.path). Error: \(error.localizedDescription)")
-                return nil
+                throw UploadError.fileDeletionFailed
             }
         }
         
         // Create directory if needed
-        if !_fileManager.fileExists(atPath: uploaderDirectory.path) {
+        if !fileManager.fileExists(atPath: uploaderDirectory.path) {
             do {
-                try _fileManager.createDirectory(at: uploaderDirectory, withIntermediateDirectories: false, attributes: nil)
+                try fileManager.createDirectory(at: uploaderDirectory, withIntermediateDirectories: false, attributes: nil)
             } catch {
                 print("Cannot save data at path \(filePath.path). Error: \(error.localizedDescription)")
-                return nil
+                throw UploadError.directoryCreationFailed
             }
         }
         
-        // Save NSData to file
+        // Save Data to file
         do {
             try data.write(to: filePath, options: .atomic)
         } catch {
             print("Cannot save data at path \(filePath.path). Error: \(error.localizedDescription)")
-            return nil
+            throw UploadError.dataSavingFailed
         }
         
         return filePath
     }
     
     func removeFilesForUpload(_ uploadId: String) {
-
+        
         if let fileURL = _filesMap[uploadId] {
             do {
                 try FileManager.default.removeItem(at: fileURL)
@@ -102,20 +103,20 @@ public class FileUploaderService: NSObject, URLSessionDelegate {
         
         return _urlSession!
     }
-
-
+    
+    
     // MARK: - URLSessionDelegate
-
+    
     func urlSession(_ session: URLSession,
                     task: URLSessionTask,
                     didCompleteWithError error: Error?) {
-
+        
         guard let taskDescription = task.taskDescription else {
             return
         }
         
         var data: [String: Any] = ["id": taskDescription]
-
+        
         if let uploadTask = task as? URLSessionDataTask,
            let response = uploadTask.response as? HTTPURLResponse {
             data["responseCode"] = response.statusCode
@@ -134,12 +135,12 @@ public class FileUploaderService: NSObject, URLSessionDelegate {
             data["error"] = error.localizedDescription
             
             let eventName = (error as NSError).code == NSURLErrorCancelled ? "RNFileUploader-cancelled" : "RNFileUploader-error"
-//            _sendEvent(withName: eventName, body: data)
+            //            _sendEvent(withName: eventName, body: data)
         } else {
-//            _sendEvent(withName: "RNFileUploader-completed", body: data)
+            //            _sendEvent(withName: "RNFileUploader-completed", body: data)
         }
     }
-
+    
     
     
     func urlSession(_ session: URLSession, task: URLSessionTask, didSendBodyData bytesSent: Int64, totalBytesSent: Int64, totalBytesExpectedToSend: Int64) {
@@ -156,13 +157,13 @@ public class FileUploaderService: NSObject, URLSessionDelegate {
         
         //_sendEvent(withName: "RNFileUploader-progress", body: bodyData)
     }
-
+    
     
     func urlSession(_ session: URLSession, dataTask: URLSessionDataTask, didReceive data: Data) {
         guard data.count > 0 else {
             return
         }
-
+        
         // Hold returned data so it can be picked up by the didCompleteWithError method later
         if var responseData = _responsesData[dataTask.taskIdentifier] {
             responseData.append(data)
@@ -171,7 +172,7 @@ public class FileUploaderService: NSObject, URLSessionDelegate {
             _responsesData[dataTask.taskIdentifier] = responseData
         }
     }
-
+    
     
     func urlSession(_ session: URLSession, task: URLSessionTask, needNewBodyStream completionHandler: @escaping (InputStream?) -> Void) {
         let inputStream = task.originalRequest?.httpBodyStream
@@ -179,51 +180,51 @@ public class FileUploaderService: NSObject, URLSessionDelegate {
         completionHandler(inputStream)
     }
     
-//    func isEqual(_ object: Any?) -> Bool {
-//        //
-//    }
-//
-//    var hash: Int = 0
-//
-//    var superclass: AnyClass?
-//
-//    func `self`() -> Self {
-//        //
-//    }
-//
-//    func perform(_ aSelector: Selector!) -> Unmanaged<AnyObject>! {
-//        //
-//    }
-//
-//    func perform(_ aSelector: Selector!, with object: Any!) -> Unmanaged<AnyObject>! {
-//        //
-//    }
-//
-//    func perform(_ aSelector: Selector!, with object1: Any!, with object2: Any!) -> Unmanaged<AnyObject>! {
-//        //
-//    }
-//
-//    func isProxy() -> Bool {
-//        //
-//    }
-//
-//    func isKind(of aClass: AnyClass) -> Bool {
-//        //
-//    }
-//
-//    func isMember(of aClass: AnyClass) -> Bool {
-//        //
-//    }
-//
-//    func conforms(to aProtocol: Protocol) -> Bool {
-//        //
-//    }
-//
-//    func responds(to aSelector: Selector!) -> Bool {
-//        //
-//    }
-//
-//    var description: String = ""
+    //    func isEqual(_ object: Any?) -> Bool {
+    //        //
+    //    }
+    //
+    //    var hash: Int = 0
+    //
+    //    var superclass: AnyClass?
+    //
+    //    func `self`() -> Self {
+    //        //
+    //    }
+    //
+    //    func perform(_ aSelector: Selector!) -> Unmanaged<AnyObject>! {
+    //        //
+    //    }
+    //
+    //    func perform(_ aSelector: Selector!, with object: Any!) -> Unmanaged<AnyObject>! {
+    //        //
+    //    }
+    //
+    //    func perform(_ aSelector: Selector!, with object1: Any!, with object2: Any!) -> Unmanaged<AnyObject>! {
+    //        //
+    //    }
+    //
+    //    func isProxy() -> Bool {
+    //        //
+    //    }
+    //
+    //    func isKind(of aClass: AnyClass) -> Bool {
+    //        //
+    //    }
+    //
+    //    func isMember(of aClass: AnyClass) -> Bool {
+    //        //
+    //    }
+    //
+    //    func conforms(to aProtocol: Protocol) -> Bool {
+    //        //
+    //    }
+    //
+    //    func responds(to aSelector: Selector!) -> Bool {
+    //        //
+    //    }
+    //
+    //    var description: String = ""
     
     
 }
