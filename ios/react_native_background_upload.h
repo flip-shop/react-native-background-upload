599c107b7582ad842e6aef81348fbb79b760ba8f
Michael Czerniakowski <michal.czerniakowski@flip.shop>
2023-05-25 20:03:37 +0200

    Add test header file.

---
+++ ios/react_native_background_upload.h
@@ -0,0 +1,12 @@
+//
+//  react_native_background_upload.h
+//  react-native-background-upload
+//
+//  Created by Michael Czerniakowski on 25/05/2023.
+//
+
+#ifndef react_native_background_upload_h
+#define react_native_background_upload_h
+
+
+#endif /* react_native_background_upload_h */
