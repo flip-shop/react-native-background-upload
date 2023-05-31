e8b37b94f9a2d6ff9a8b342217ca236df19f5596
Michael Czerniakowski <michal.czerniakowski@flip.shop>
2023-05-31 14:39:04 +0200

    Remove redundant comments from Obj-C Source.

--- ios/VydiaRNFileUploader.m
+++ ios/VydiaRNFileUploader.m
@@ -26,36 +26,15 @@ RCT_EXPORT_MODULE();
   return self;
 }
 
-/*
- Gets file information for the path specified.  Example valid path is: file:///var/mobile/Containers/Data/Application/3C8A0EFB-A316-45C0-A30A-761BF8CCF2F8/tmp/trim.A5F76017-14E9-4890-907E-36A045AF9436.MOV
- Returns an object such as: {mimeType: "video/quicktime", size: 2569900, exists: true, name: "trim.AF9A9225-FC37-416B-A25B-4EDB8275A625.MOV", extension: "MOV"}
- */
-
 RCT_EXPORT_METHOD(getFileInfo:(NSString *)path resolve:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject)
 {
     [fileUploader getFileInfo:path resolve:resolve reject:reject];
 }
 
-/*
- * Starts a file upload.
- * Options are passed in as the first argument as a js hash:
- * {
- *   url: string.  url to post to.
- *   path: string.  path to the file on the device
- *   headers: hash of name/value header pairs
- * }
- *
- * Returns a promise with the string ID of the upload.
- */
 RCT_EXPORT_METHOD(startUpload:(NSDictionary *)options resolve:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject) {
     [fileUploader startUpload:options resolve:resolve reject:reject];
 }
 
-/*
- * Cancels file upload
- * Accepts upload ID as a first argument, this upload will be cancelled
- * Event "cancelled" will be fired when upload is cancelled.
- */
 RCT_EXPORT_METHOD(cancelUpload: (NSString *)cancelUploadId resolve:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject) {
     [fileUploader cancelUpload:cancelUploadId resolve:resolve reject:reject];
 }
