763533cb9fee783a1b2aaa24e39e3ef44bc9b6fd
Michael Czerniakowski <michal.czerniakowski@flip.shop>
2023-06-01 11:54:17 +0200

    Refactor and remove unnecessary code from main file.

--- ios/VydiaRNFileUploader.m
+++ ios/VydiaRNFileUploader.m
@@ -1,39 +1,14 @@
-#import <Foundation/Foundation.h>
-#import <React/RCTEventEmitter.h>
-#import <React/RCTBridgeModule.h>
+//#import <React/RCTEventEmitter.h>
 #import "react_native_background_upload-Swift.h"
+#import <React/RCTBridgeModule.h>
 
-@interface VydiaRNFileUploader : RCTEventEmitter <RCTBridgeModule, NSURLSessionTaskDelegate>
-{
-    FileUploaderService *fileUploader;
-}
-@end
-
-@implementation VydiaRNFileUploader
-
-RCT_EXPORT_MODULE();
-
-@synthesize bridge = _bridge;
-
--(id) init {
-  self = [super init];
-  if (self) {
-      fileUploader = [FileUploaderService init];
-  }
-  return self;
-}
+@interface RCT_EXTERN_MODULE(VydiaRNFileUploader, NSObject)
 
 RCT_EXPORT_METHOD(getFileInfo:(NSString *)path resolve:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject)
-{
-    [fileUploader getFileInfo:path resolve:resolve reject:reject];
-}
 
-RCT_EXPORT_METHOD(startUpload:(NSDictionary *)options resolve:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject) {
-    [fileUploader startUpload:options resolve:resolve reject:reject];
-}
+RCT_EXPORT_METHOD(startUpload:(NSDictionary *)options resolve:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject)
 
-RCT_EXPORT_METHOD(cancelUpload: (NSString *)cancelUploadId resolve:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject) {
-    [fileUploader cancelUpload:cancelUploadId resolve:resolve reject:reject];
-}
+RCT_EXPORT_METHOD(cancelUpload: (NSString *)cancelUploadId resolve:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject)
 
 @end
+
