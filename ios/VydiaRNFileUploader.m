02723f91054e785b0b0b17463890fee0406ee73d
Michael Czerniakowski <michal.czerniakowski@flip.shop>
2023-06-07 01:03:25 +0200

    Update imports

--- ios/VydiaRNFileUploader.m
+++ ios/VydiaRNFileUploader.m
@@ -1,6 +1,7 @@
-#import <React/RCTBridgeModule.h>
+#import "React/RCTBridgeModule.h"
+#import "React/RCTEventEmitter.h"
 
-@interface RCT_EXTERN_MODULE(VydiaRNFileUploader, NSObject)
+@interface RCT_EXTERN_MODULE(VydiaRNFileUploader, RCTEventEmitter)
 
 RCT_EXTERN_METHOD(getFileInfo:(NSString *)path resolve:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject)
 
