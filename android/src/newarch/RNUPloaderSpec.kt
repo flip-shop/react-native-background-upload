18c33e04b2b5bb2fd6508af34d54e2b23448e262
Krzysztof Gajewski <krzysztof.gajewski@flip.shop>
2024-11-15 12:21:05 +0100

    working

--- android/src/newarch/RNUPloaderSpec.kt
+++ android/src/newarch/RNUPloaderSpec.kt
@@ -1,6 +1,7 @@
 package com.vydia.RNUploader
 
 import com.facebook.react.bridge.ReactApplicationContext
+import com.vydia.NativeRNUploaderSpec
 
 abstract class RNUploaderSpec internal constructor(context: ReactApplicationContext) :
   NativeRNUploaderSpec(context) {
--- src/NativeRNUploader.ts
+++ src/NativeRNUploader.ts
@@ -1,11 +1,10 @@
-import type { TurboModule } from 'react-native';
-import { TurboModuleRegistry } from 'react-native';
+import { TurboModule, TurboModuleRegistry } from 'react-native';
 
 export interface Spec extends TurboModule {
-  getFileInfo(path: string): Promise<Object>
-  startUpload(options: Object): Promise<boolean>
-  cancelUpload(cancelUploadId: string): Promise<boolean>
-  stopAllUploads(): Promise<boolean>
+  getFileInfo(path: string): Promise<Object>;
+  startUpload(options: Object): Promise<boolean>;
+  cancelUpload(cancelUploadId: string): Promise<boolean>;
+  stopAllUploads(): Promise<boolean>;
 }
 
 export default TurboModuleRegistry.getEnforcing<Spec>('RNUploaderModule');
