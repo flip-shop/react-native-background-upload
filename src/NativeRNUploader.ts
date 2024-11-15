ef32c140c8eb12313487c6566b4e8b4366a39561
Krzysztof Gajewski <krzysztof.gajewski@flip.shop>
2024-11-15 11:39:11 +0100

    changed object -> Object

--- src/NativeRNUploader.ts
+++ src/NativeRNUploader.ts
@@ -2,8 +2,8 @@ import type { TurboModule } from 'react-native';
 import { TurboModuleRegistry } from 'react-native';
 
 export interface Spec extends TurboModule {
-  getFileInfo(path: string): Promise<object>
-  startUpload(options: object): Promise<boolean>
+  getFileInfo(path: string): Promise<Object>
+  startUpload(options: Object): Promise<boolean>
   cancelUpload(cancelUploadId: string): Promise<boolean>
   stopAllUploads(): Promise<boolean>
 }
